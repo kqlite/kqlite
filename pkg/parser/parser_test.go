f5e6920d770aa258cdfa6136302b51de48e395f3
Emil Penchev <tickbg@gmail.com>
2025-01-10 16:43:38 +0200

    Parser improved, fixed bugs and added some new features, additional unit tests added.

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -30,13 +30,37 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		Expect(result[0].Args).NotTo(BeEmpty())
-		Expect(result[0].Args[0]).To(Equal("id"))
-		Expect(result[0].Args[1]).To(Equal("id"))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns[0]).To(Equal("id"))
+		Expect(result[0].ArgColumns[1]).To(Equal("id"))
+
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(1))
 		Expect(result[0].Tables[0]).To(Equal("kine"))
-		Expect(result[0].Tables[1]).To(Equal("kine"))
-		Expect(result[0].Tables[2]).To(Equal("kine"))
+	})
+
+	It("Parse SELECT Statement with JOIN", func() {
+		sql := `SELECT orders.order_id, suppliers.name   
+				FROM suppliers  
+				INNER JOIN orders  
+				ON suppliers.supplier_id = orders.supplier_id  
+				ORDER BY order_id`
+		result, err := parser.Parse(sql)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result).NotTo(BeEmpty())
+		Expect(result).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).To(BeEmpty())
+
+		By("Verify query tables")
+		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(2))
+		Expect(result[0].Tables[0]).To(Equal("suppliers"))
+		Expect(result[0].Tables[1]).To(Equal("orders"))
 	})
 
 	It("Parse SELECT Statement", func() {
@@ -46,9 +70,13 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		Expect(result[0].Args).NotTo(BeEmpty())
-		Expect(result[0].Args[0]).To(Equal("income"))
-		Expect(result[0].Args[1]).To(Equal("age"))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns[0]).To(Equal("income"))
+		Expect(result[0].ArgColumns[1]).To(Equal("age"))
+
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
 		Expect(result[0].Tables[0]).To(Equal("employees"))
 	})
@@ -62,15 +90,19 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		// Parser condideres virtual tables like 'tables' as real table reference.
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns).To(HaveLen(2))
+		Expect(result[0].ArgColumns[0]).To(Equal("type"))
+		Expect(result[0].ArgColumns[1]).To(Equal("tablename"))
+
+		// Parser considers virtual tables like 'tables' as real table reference.
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
 		Expect(result[0].Tables).To(HaveLen(2))
-		Expect(result[0].Args).NotTo(BeEmpty())
-		Expect(result[0].Args).To(HaveLen(2))
 		Expect(result[0].Tables[0]).To(Equal("tables"))
 		Expect(result[0].Tables[1]).To(Equal("sqlite_master"))
-		Expect(result[0].Args[0]).To(Equal("type"))
-		Expect(result[0].Args[1]).To(Equal("tablename"))
 	})
 
 	It("Parse INSERT Statement", func() {
@@ -80,22 +112,27 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns).To(HaveLen(8))
+		Expect(result[0].ArgColumns[0]).To(Equal("name"))
+		Expect(result[0].ArgColumns[1]).To(Equal("created"))
+		Expect(result[0].ArgColumns[2]).To(Equal("deleted"))
+		Expect(result[0].ArgColumns[3]).To(Equal("create_revision"))
+		Expect(result[0].ArgColumns[4]).To(Equal("prev_revision"))
+		Expect(result[0].ArgColumns[5]).To(Equal("lease"))
+		Expect(result[0].ArgColumns[6]).To(Equal("value"))
+		Expect(result[0].ArgColumns[7]).To(Equal("old_value"))
+
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
 		Expect(result[0].Tables).To(HaveLen(1))
 		Expect(result[0].Tables[0]).To(Equal("kine"))
-		Expect(result[0].Args).NotTo(BeEmpty())
-		Expect(result[0].Args).To(HaveLen(8))
-		Expect(result[0].Args[0]).To(Equal("name"))
-		Expect(result[0].Args[1]).To(Equal("created"))
-		Expect(result[0].Args[2]).To(Equal("deleted"))
-		Expect(result[0].Args[3]).To(Equal("create_revision"))
-		Expect(result[0].Args[4]).To(Equal("prev_revision"))
-		Expect(result[0].Args[5]).To(Equal("lease"))
-		Expect(result[0].Args[6]).To(Equal("value"))
-		Expect(result[0].Args[7]).To(Equal("old_value"))
+
 	})
 
-	It("Parse INSERT wih SELECT Statement", func() {
+	It("Parse INSERT with SELECT Statement", func() {
 		sql := `INSERT INTO Customers (CustomerName, City, Country)
 				SELECT SupplierName, City, Country FROM Suppliers
 				WHERE Country=$1`
@@ -103,13 +140,17 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		Expect(result[0].Args).NotTo(BeEmpty())
-		Expect(result[0].Args).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns).To(HaveLen(1))
+		Expect(result[0].ArgColumns[0]).To(Equal("country"))
+
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
 		Expect(result[0].Tables).To(HaveLen(2))
 		Expect(result[0].Tables[0]).To(Equal("customers"))
 		Expect(result[0].Tables[1]).To(Equal("suppliers"))
-		Expect(result[0].Args[0]).To(Equal("country"))
 	})
 
 	It("Parse UPDATE Statement", func() {
@@ -119,14 +160,17 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		Expect(result[0].Args).To(HaveLen(1))
-		Expect(result[0].Tables).NotTo(BeEmpty())
-		Expect(result[0].Tables).To(HaveLen(3))
 
-		Expect(result[0].Args[0]).To(Equal("title"))
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).To(HaveLen(2))
+		Expect(result[0].ArgColumns[0]).To(Equal("primary_author"))
+		Expect(result[0].ArgColumns[1]).To(Equal("title"))
+
+		By("Verify query tables")
+		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(2))
 		Expect(result[0].Tables[0]).To(Equal("books"))
-		Expect(result[0].Tables[1]).To(Equal("books"))
-		Expect(result[0].Tables[2]).To(Equal("authors"))
+		Expect(result[0].Tables[1]).To(Equal("authors"))
 	})
 
 	It("Parse UPDATE with SELECT Statement", func() {
@@ -138,12 +182,83 @@ var _ = Describe("Parser tests", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).NotTo(BeEmpty())
 		Expect(result).To(HaveLen(1))
-		Expect(result[0].Args).NotTo(BeEmpty())
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).NotTo(BeEmpty())
+		Expect(result[0].ArgColumns).To(HaveLen(1))
+		Expect(result[0].ArgColumns[0]).To(Equal("personid"))
+
+		By("Verify query tables")
 		Expect(result[0].Tables).NotTo(BeEmpty())
-		Expect(result[0].Args).To(HaveLen(1))
 		Expect(result[0].Tables).To(HaveLen(2))
 		Expect(result[0].Tables[0]).To(Equal("persons"))
 		Expect(result[0].Tables[1]).To(Equal("addresslist"))
-		Expect(result[0].Args[0]).To(Equal("personid"))
+	})
+
+	It("Parse simple UPDATE Statement", func() {
+		sql := `UPDATE _litestream_seq SET seq = $1 WHERE id = $2`
+		result, err := parser.Parse(sql)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result).NotTo(BeEmpty())
+		Expect(result).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).To(HaveLen(2))
+		Expect(result[0].ArgColumns[0]).To(Equal("seq"))
+		Expect(result[0].ArgColumns[1]).To(Equal("id"))
+
+		By("Verify query tables")
+		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(1))
+		Expect(result[0].Tables[0]).To(Equal("_litestream_seq"))
+	})
+
+	It("Parse UPDATE Statement with multiple columns", func() {
+		sql := `UPDATE seq_number SET seq = $1, double = $2, triple = $3 WHERE id = $4`
+		result, err := parser.Parse(sql)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result).NotTo(BeEmpty())
+		Expect(result).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).To(HaveLen(4))
+		Expect(result[0].ArgColumns[0]).To(Equal("seq"))
+		Expect(result[0].ArgColumns[1]).To(Equal("double"))
+		Expect(result[0].ArgColumns[2]).To(Equal("triple"))
+		Expect(result[0].ArgColumns[3]).To(Equal("id"))
+
+		By("Verify query tables")
+		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(1))
+		Expect(result[0].Tables[0]).To(Equal("seq_number"))
+	})
+
+	It("Parse SELECT with CASE WHEN statement", func() {
+		sql := `SELECT
+					trackid,
+					name,
+					CASE
+						WHEN milliseconds < $1 THEN
+							'short'
+						WHEN milliseconds > $2 AND milliseconds < $3 THEN 'medium'
+						ELSE
+							'long'
+						END category
+				FROM tracks;`
+		result, err := parser.Parse(sql)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result).NotTo(BeEmpty())
+		Expect(result).To(HaveLen(1))
+
+		By("Verify query args/parameters")
+		Expect(result[0].ArgColumns).To(HaveLen(3))
+		Expect(result[0].ArgColumns[0]).To(Equal("milliseconds"))
+		Expect(result[0].ArgColumns[1]).To(Equal("milliseconds"))
+		Expect(result[0].ArgColumns[2]).To(Equal("milliseconds"))
+
+		By("Verify query tables")
+		Expect(result[0].Tables).NotTo(BeEmpty())
+		Expect(result[0].Tables).To(HaveLen(1))
+		Expect(result[0].Tables[0]).To(Equal("tracks"))
 	})
 })
