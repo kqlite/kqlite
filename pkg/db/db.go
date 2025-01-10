4a2145ace8da0a81af006d5964ddff30018bbcd4
Emil Penchev <tickbg@gmail.com>
2025-01-10 16:59:02 +0200

    DB layer improved, handling transactions, automated response creation for read and write queries, added single entry point for executing SQL statements and many other improvements.

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -33,6 +33,42 @@ type DB struct {
 	rodsn string // DSN used for read-only connections
 }
 
+type execerQueryer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+}
+
+// Execute queries in a connection context, holds the current transaction specific to the client connection.
+type ExecuteQueryContext struct {
+	db  *DB
+	tx  *sql.Tx
+	ctx context.Context
+}
+
+type ExecuteQueryResponse struct {
+	Rows       *sql.Rows
+	CommandTag string
+	Error      error
+}
+
+type SqlCmdType string
+
+const (
+	CMD_SELECT   SqlCmdType = "SELECT"
+	CMD_UPDATE   SqlCmdType = "UPDATE"
+	CMD_INSERT   SqlCmdType = "INSERT"
+	CMD_DELETE   SqlCmdType = "DELETE"
+	CMD_BEGIN    SqlCmdType = "BEGIN"
+	CMD_COMMIT   SqlCmdType = "COMMIT"
+	CMD_ROLLBACK SqlCmdType = "ROLLBACK"
+)
+
+type Statement struct {
+	Query      string
+	CmdType    SqlCmdType
+	Parameters []any
+}
+
 // Open opens a file-based database using the default driver.
 func Open(dbPath string, fkEnabled, wal bool) (retDB *DB, retErr error) {
 	return openSQLiteDB(dbPath, false, false)
@@ -192,24 +228,39 @@ func (db *DB) Close() error {
 	if err := db.rwdb.Close(); err != nil {
 		return err
 	}
+
 	return db.rodb.Close()
 }
 
-// A tiny wraper around DB.ExecContext.
+// A tiny wrapper around DB.ExecContext.
 // Executes a query without returning any rows. The args are for any placeholder parameters in the query.
 func (db *DB) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
-	if query != "" {
-		return db.rwdb.ExecContext(ctx, query, args...)
+	conn, err := db.rwdb.Conn(ctx)
+	if err != nil {
+		return nil, err
 	}
+	defer conn.Close()
+
+	if query != "" {
+		return conn.ExecContext(ctx, query, args...)
+	}
+
 	return nil, nil
 }
 
-// A tiny wraper around DB.QueryContext.
+// A tiny wrapper around DB.QueryContext.
 // Executes a query that returns rows, typically a SELECT. The args are for any placeholder parameters in the query.
 func (db *DB) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	conn, err := db.rodb.Conn(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
 	if query != "" {
 		return db.rodb.QueryContext(ctx, query, args...)
 	}
+
 	return nil, nil
 }
 
@@ -222,6 +273,7 @@ func (db *DB) StmtReadOnly(sql string) (bool, error) {
 		return false, err
 	}
 	defer conn.Close()
+
 	return db.StmtReadOnlyWithConn(sql, conn)
 }
 
@@ -244,5 +296,127 @@ func (db *DB) StmtReadOnlyWithConn(sql string, conn *sql.Conn) (bool, error) {
 	if err := conn.Raw(f); err != nil {
 		return false, err
 	}
+
 	return readOnly, nil
+}
+
+// Handle transaction commands separately from the other common SQL commands.
+func (qcontext *ExecuteQueryContext) handleTransaction(stmt Statement) (bool, error) {
+	var err error
+	var handled bool
+
+	switch stmt.CmdType {
+	case CMD_BEGIN: // Check for transaction start
+		handled = true
+		if qcontext.tx != nil {
+			return handled, fmt.Errorf("Syntax error, an active transaction is already present")
+		}
+		if qcontext.tx, err = qcontext.db.rwdb.BeginTx(qcontext.ctx, nil); err != nil {
+			return handled, err
+		}
+		break
+
+	case CMD_COMMIT: // Check for transaction end
+		handled = true
+		if qcontext.tx == nil {
+			return handled, fmt.Errorf("No active transaction to COMMIT")
+		}
+
+		if err := qcontext.tx.Commit(); err != nil {
+			return handled, err
+		}
+		qcontext.tx = nil
+		break
+
+	case CMD_ROLLBACK: // Check for transaction abort/rollback
+		handled = true
+		if qcontext.tx == nil {
+			return handled, fmt.Errorf("No active transaction to ROLLBACK")
+		}
+		if err := qcontext.tx.Rollback(); err != nil {
+			return handled, err
+		}
+		qcontext.tx = nil
+		break
+	}
+
+	return handled, err
+}
+
+// Get a ExecuteQueryContext for this database.
+func (db *DB) CreateContext(ctx context.Context) *ExecuteQueryContext {
+	return &ExecuteQueryContext{
+		db:  db,
+		ctx: ctx,
+		tx:  nil,
+	}
+}
+
+// Request execution of on or more SQL statements that can contain both executes and queries.
+func (qcontext *ExecuteQueryContext) Request(statements []Statement) ([]ExecuteQueryResponse, error) {
+	var err error
+	var response []ExecuteQueryResponse
+
+	// abortOnError indicates whether the caller should continue
+	// processing or break.
+	abortOnError := func(err error) bool {
+		if err != nil && qcontext.tx != nil {
+			qcontext.tx.Rollback()
+			qcontext.tx = nil
+			return true
+		}
+		return false
+	}
+
+	// point executor to default DB connection
+	eq := execerQueryer(qcontext.db)
+
+	if qcontext.tx != nil {
+		eq = qcontext.tx
+	}
+
+	for _, stmt := range statements {
+		if handled, err := qcontext.handleTransaction(stmt); handled || err != nil {
+			response = append(response, ExecuteQueryResponse{
+				Error:      err,
+				CommandTag: fmt.Sprintf("%s", string(stmt.CmdType)),
+			})
+			return response, err
+		}
+
+		readOnly, err := qcontext.db.StmtReadOnly(stmt.Query)
+		if err != nil {
+			return response, err
+		}
+
+		if readOnly {
+			rows, err := eq.QueryContext(qcontext.ctx, stmt.Query, stmt.Parameters...)
+			response = append(response, ExecuteQueryResponse{
+				Error:      err,
+				Rows:       rows,
+				CommandTag: fmt.Sprintf("%s 1", stmt.Query),
+			})
+
+			if abortOnError(err) {
+				break
+			}
+		} else {
+			result, err := eq.ExecContext(qcontext.ctx, stmt.Query, stmt.Parameters...)
+
+			var rowsAffected int64
+			if result != nil {
+				rowsAffected, _ = result.RowsAffected()
+			}
+
+			response = append(response, ExecuteQueryResponse{
+				Error:      err,
+				CommandTag: fmt.Sprintf("%s, %d", string(stmt.CmdType), rowsAffected),
+			})
+
+			if abortOnError(err) {
+				break
+			}
+		}
+	}
+	return response, err
 }
