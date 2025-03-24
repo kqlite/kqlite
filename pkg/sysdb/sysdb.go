746e2acc4ee10d25e9cb2cba054ad49ecfa17d44
Emil Penchev <tickbg@gmail.com>
2025-03-24 11:11:43 +0200

    Added pg_total_relation_size postgresql function stub.

--- pkg/sysdb/sysdb.go
+++ pkg/sysdb/sysdb.go
@@ -3,6 +3,8 @@ package sysdb
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	sqlite3 "github.com/mattn/go-sqlite3"
@@ -16,27 +18,39 @@ func init() {
 			if err := conn.RegisterFunc("current_catalog", currentCatalog, true); err != nil {
 				return fmt.Errorf("cannot register current_catalog() function")
 			}
+
 			if err := conn.RegisterFunc("current_schema", currentSchema, true); err != nil {
 				return fmt.Errorf("cannot register current_schema() function")
 			}
+
 			if err := conn.RegisterFunc("current_user", currentUser, true); err != nil {
 				return fmt.Errorf("cannot register current_schema() function")
 			}
+
 			if err := conn.RegisterFunc("session_user", sessionUser, true); err != nil {
 				return fmt.Errorf("cannot register session_user() function")
 			}
+
 			if err := conn.RegisterFunc("user", user, true); err != nil {
 				return fmt.Errorf("cannot register user() function")
 			}
+
 			if err := conn.RegisterFunc("show", show, true); err != nil {
 				return fmt.Errorf("cannot register show() function")
 			}
+
 			if err := conn.RegisterFunc("format_type", formatType, true); err != nil {
 				return fmt.Errorf("cannot register format_type() function")
 			}
+
 			if err := conn.RegisterFunc("version", version, true); err != nil {
 				return fmt.Errorf("cannot register version() function")
 			}
+
+			if err := conn.RegisterFunc("pg_total_relation_size", pg_total_relation_size, true); err != nil {
+				return fmt.Errorf("cannot register pg_total_relation_size() function")
+			}
+
 			if err := conn.CreateModule("pg_database_module", &PGDatabaseModule{}); err != nil {
 				return fmt.Errorf("cannot register pg_database module")
 			}
@@ -52,11 +66,20 @@ func currentUser() string { return "sqlite3" }
 func sessionUser() string { return "sqlite3" }
 func user() string        { return "sqlite3" }
 
-func version() string { return "kqlite v0.0.0" }
+func version() string { return "cockroachdb" /*"kqlite v0.0.0"*/ }
 
 func formatType(type_oid, typemod string) string { return "" }
 
 func show(name string) string { return "" }
+
+// Returns Total disk space used by the specified table, including all indexes and TOAST data.
+func pg_total_relation_size(name string) int64 {
+	if finfo, err := os.Stat(filepath.Join(os.Getenv("DATA_DIR"), name)); err != nil {
+		return -1
+	} else {
+		return finfo.Size()
+	}
+}
 
 func DatabaseTypeConvSqlite(t string) int {
 	if strings.Contains(t, "INT") {
